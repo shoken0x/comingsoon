a728311716e11165bfeab40fad5a99468196b0d7
syokenz <syokenz@gmail.com>
2012-12-23 18:32:18 +0900

    change gmail address

--- lib/gconf.rb
+++ lib/gconf.rb
@@ -1,6 +1,6 @@
 class GConf
-  USERNAME='_@gmail.com'  #Gmailのアドレス
-  PASSWORD='xxxx'     #Gmailのパスワード
+  USERNAME='mail@kitchhike.com'  #Gmailのアドレス
+  PASSWORD='xxxx'                #Gmailのパスワード
   SUBJECT='Welcome KitchHike!!'
   BODY='KitchHike is coming soon...  Leave your email for early access.'
 end
