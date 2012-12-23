24d802402a7806ee96b11baa65a664e715b4bea4
syokenz <syokenz@gmail.com>
2012-12-24 03:02:31 +0900

    add insert params

--- server.rb
+++ server.rb
@@ -8,7 +8,13 @@ get '/' do
 end
 
 post '/result' do
-  Email.save ({:email => params[:email]})
+  Email.save ({:email                => params[:email],
+               :HTTP_ACCEPT_LANGUAGE => request.env["HTTP_ACCEPT_LANGUAGE"],
+               :HTTP_USER_AGENT      => request.env["HTTP_USER_AGENT"],
+               :HTTP_X_REAL_IP       => request.env["HTTP_X_REAL_IP"],
+               :REMOTE_ADDR          => request.env["REMOTE_ADDR"],
+               :create_at            => Time.now
+  })
   Email.send params[:email]
   erb :result
 end
