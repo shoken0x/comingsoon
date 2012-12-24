a0bc1b69512245d9cfbee154cb92dfedb3daa77c
syokenz <syokenz@gmail.com>
2012-12-24 14:42:12 +0900

    change message

--- lib/message.rb
+++ lib/message.rb
@@ -2,6 +2,6 @@ require 'pit'
 
 class Message
   WELCOME='KitchHike is coming soon...<br />Leave your email for early access.'
-  THANKYOU='Thank you!'
-  NOTVALID='Not Valid Email'
+  THANKYOU='Thank you! <br />You will soon receive an email. <br /> <span class="text-red">If not receive,</span> Please contact us. mail@kitchhike.com'
+  NOTVALID='<span class="text-red">Not Valid Email</span><br /><br />'
 end
